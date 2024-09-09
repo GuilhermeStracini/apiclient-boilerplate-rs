16f316cc9392f96f19137e73b08cf3c953fab432
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-09 22:48:51 +0100

    Update post.rs

--- src/bin/post.rs
+++ src/bin/post.rs
@@ -9,10 +9,10 @@ async fn main() -> Result<(), reqwest::Error> {
 
      match api_client.get_post(1).await {
         Ok(post) => {
-            println!("Post: {:?}", post);
+            println!("Post: {post:?}");
         }
         Err(err) => {
-            eprintln!("Error: {}", err);
+            eprintln!("Error: {err}");
         }
     }
 
