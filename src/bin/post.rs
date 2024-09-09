297b4a4927fb10181a629cc9d4d5196228d48949
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-09 14:11:37 +0100

    Update post.rs

--- src/bin/post.rs
+++ src/bin/post.rs
@@ -5,7 +5,7 @@ use apiclient_boilerplate_rust::client::ApiClient;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let base_url = "https://jsonplaceholder.typicode.com";
-    let api_client = ApiClient::new(base_url);
+    let api_client = ApiClient::new(&base_url);
 
      match api_client.get_post(1).await {
         Ok(post) => {
