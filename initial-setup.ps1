d41cba9b6b7ca3e23f4c67e9260a5dfb718f77ca
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-09 13:23:56 +0100

    Update initial-setup.ps1

--- initial-setup.ps1
+++ initial-setup.ps1
@@ -4,16 +4,22 @@ $GitHubUsername = Read-Host -Prompt 'GitHub username'
 $GitHubRepo = Read-Host -Prompt 'GitHub repository'
 $AppVeyorId = Read-Host -Prompt 'AppVeyor project ID'
 
-(Get-Content README.md) | Select-Object -Skip 20 | Set-Content README.md
+Remove-Item README.md
+Rename-Item -Path ".\README.template.md" -NewName ".\README.md"
 
 (Get-Content README.md) | ForEach-Object { $_ -replace "{username}", $GitHubUsername } | Set-Content README.md
 (Get-Content README.md) | ForEach-Object { $_ -replace "{repo}", $GitHubRepo } | Set-Content README.md
 (Get-Content README.md) | ForEach-Object { $_ -replace "{appVeyorId}", $AppVeyorId } | Set-Content README.md
 (Get-Content README.md) | ForEach-Object { $_ -replace "API Client Boilerplate", $ProjectName } | Set-Content README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "{Project Description}", $ProjectDescription } | Set-Content README.md
+(Get-Content README.md) | ForEach-Object { $_ -replace "https://project.name.com/", $DocumentationWebsite } | Set-Content README.md
 
-(Get-Content _config.yml) | ForEach-Object { $_ -replace "API Client Boilerplate .NET", "$ProjectName Rust SDK Client" } | Set-Content _config.yml
-(Get-Content _config.yml) | ForEach-Object { $_ -replace "A template repository for Rust API clients projects.", $ProjectDescription } | Set-Content _config.yml
+(Get-Content .wakatime-project) | ForEach-Object { $_ -replace "API Client Boilerplate .NET", "$ProjectName SDK .NET" } | Set-Content .wakatime-project
+
+(Get-Content _config.yml) | ForEach-Object { $_ -replace "API Client Boilerplate", $ProjectName } | Set-Content _config.yml
+(Get-Content _config.yml) | ForEach-Object { $_ -replace "A template repository for RUST API clients projects.", $ProjectDescription } | Set-Content _config.yml
 (Get-Content _config.yml) | ForEach-Object { $_ -replace "GuilhermeStracini/apiclient-boilerplate-rs", "$GitHubUsername/$GitHubRepo" } | Set-Content _config.yml
 
 Remove-Item initial-setup.bat
 Remove-Item initial-setup.ps1
+Remove-Item initial-setup.sh
